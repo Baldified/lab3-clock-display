2ead8c0e7b14a31d9dbd04ecc42eae3c5e551b3b
Bruce <l4m.bruc3@gmail.com>
2025-09-09 22:42:46 -0400

    working on step 36

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -16,15 +16,14 @@ public class ClockDisplay
 {
     private NumberDisplay hours;
     private NumberDisplay minutes;
-    private String displayString;    // simulates the actual display
-    
+    private String displayString;    // simulates the actual display  
     /**
      * Constructor for ClockDisplay objects. This constructor 
      * creates a new clock set at 00:00.
      */
     public ClockDisplay()
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
         updateDisplay();
     }
@@ -36,7 +35,7 @@ public class ClockDisplay
      */
     public ClockDisplay(int hour, int minute)
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
         setTime(hour, minute);
     }
--- NumberDisplay.java
+++ NumberDisplay.java
@@ -42,6 +42,7 @@ public class NumberDisplay
     {
         if(value < 10) {
             return "0" + value;
+
         }
         else {
             return "" + value;
